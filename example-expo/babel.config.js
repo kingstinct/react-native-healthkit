56aac348dee03096feb0f9b8bb0d47b64ca5a70e
Robert Herber <robert@kingstinct.com>
2022-08-29 10:32:46 +0200

    chore: merge example projects

--- example-expo/babel.config.js
+++ example-expo/babel.config.js
@@ -8,8 +8,11 @@ module.exports = {
         extensions: ['.js', '.ts', '.tsx'],
         alias: {
           '@kingstinct/react-native-healthkit': '../src/index',
+          'example': '../example',
+          'example/src/App': '../example/src/App',
           'react': './node_modules/react',
           'react-native': './node_modules/react-native',
+          'react-native-paper': './node_modules/react-native-paper',
           '@babel': './node_modules/@babel',
           'fbjs': './node_modules/fbjs',
           'hoist-non-react-statics': './node_modules/hoist-non-react-statics',
