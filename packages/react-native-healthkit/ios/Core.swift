bbd9e45b8bbc2e4cccc1e9a8fab9c0efc909fc84
Robert Herber <robert@kingstinct.com>
2025-06-01 20:02:27 +0200

    implement more stuff

--- packages/react-native-healthkit/ios/Core.swift
+++ packages/react-native-healthkit/ios/Core.swift
@@ -8,6 +8,20 @@ import NitroModules
 //
 
 class Core : HybridCoreSpec {
+    func unsubscribeQueryAsync(queryId: String) throws -> Promise<Bool> {
+        let result = try self.unsubscribeQuery(queryId: queryId)
+        
+        return Promise.resolved(withResult: result)
+    }
+    
+    func isHealthDataAvailableAsync() -> Promise<Bool> {
+        return Promise.resolved(withResult: HKHealthStore.isHealthDataAvailable())
+    }
+    
+    func isProtectedDataAvailableAsync() -> Promise<Bool> {
+        return Promise.resolved(withResult: UIApplication.shared.isProtectedDataAvailable)
+    }
+    
     func isHealthDataAvailable() throws -> Bool {
         return HKHealthStore.isHealthDataAvailable()
     }
@@ -16,21 +30,104 @@ class Core : HybridCoreSpec {
         return UIApplication.shared.isProtectedDataAvailable
     }
     
-    func getPreferredUnits(identifiers: [QuantityTypeIdentifier]) throws -> Promise<Dictionary<String, String>> {
-        return Promise.resolved(withResult: [:])
+    func getPreferredUnits(identifiers: [String]) throws -> Promise<[IdentifierWithUnit]> {
+        return Promise.async {
+            try await withCheckedThrowingContinuation { continuation in
+                var quantityTypes = Set<HKQuantityType>()
+                    for identifierString in identifiers {
+                      let identifier = HKQuantityTypeIdentifier.init(rawValue: identifierString)
+                      let type = HKSampleType.quantityType(forIdentifier: identifier)
+                      if type != nil {
+                        quantityTypes.insert(type!)
+                      }
+                    }
+
+                    store.preferredUnits(for: quantityTypes) {
+                      (typePerUnits: [HKQuantityType: HKUnit], _: Error?) in
+                      
+                        
+                        let dic = typePerUnits.map { typePerUnit in
+                            return IdentifierWithUnit(
+                                typeIdentifier: typePerUnit.key.identifier,
+                                unit: typePerUnit.value.unitString
+                            )
+                        }
+
+                      continuation.resume(returning: dic)
+                    }
+                    
+                }
+            }
+            
+        
+        
     }
     
-    func addListener(callback: @escaping (OnChangeCallbackArgs) -> Void) throws -> EmitterSubscription {
-        return EmitterSubscription()
+    var _runningQueries: [String: HKQuery] = [:]
+    
+    func subscribeToObserverQuery(typeIdentifier: String, callback: @escaping (OnChangeCallbackArgs) -> Void) throws -> String {
+        guard let sampleType = sampleTypeFromString(typeIdentifier: typeIdentifier) else {
+            throw RuntimeError.error(withMessage: "Failed to initialize " + typeIdentifier)
+        }
+
+        let predicate = HKQuery.predicateForSamples(
+          withStart: Date.init(),
+          end: nil,
+          options: HKQueryOptions.strictStartDate
+        )
+
+        let queryId = UUID().uuidString
+
+        func responder(
+          query: HKObserverQuery,
+          handler: @escaping HKObserverQueryCompletionHandler,
+          error: Error?
+        ) {
+          if error == nil {
+              DispatchQueue.main.async {
+                  callback(OnChangeCallbackArgs(typeIdentifier: typeIdentifier, errorMessage: error?.localizedDescription))
+              /*if self.bridge != nil && self.bridge.isValid {
+                self.sendEvent(
+                  withName: "onChange",
+                  body: [
+                    "typeIdentifier": typeIdentifier
+                  ])
+              }*/
+
+            }
+            handler()
+          }
+        }
+
+        let query = HKObserverQuery(
+          sampleType: sampleType,
+          predicate: predicate
+        ) {
+          (query: HKObserverQuery, handler: @escaping HKObserverQueryCompletionHandler, error: Error?)
+          in
+          
+        return responder(query: query, handler: handler, error: error)
+          
+        }
+
+        store.execute(query)
+
+        self._runningQueries.updateValue(query, forKey: queryId)
+
+        //resolve(queryId)
+        
+        return queryId
     }
     
-    func subscribeToObserverQuery(identifier: String) throws -> Promise<String> {
-        return Promise.resolved(withResult: "")
+    func unsubscribeQuery(queryId: String) throws -> Bool {
+        guard let query = self._runningQueries[queryId] else {
+            throw RuntimeError.error(withMessage: "Query with id \(queryId) not found")
+        }
+
+        store.stop(query)
+
+        self._runningQueries.removeValue(forKey: queryId)
+
+        return true
     }
-    
-    func unsubscribeQuery(queryId: String) throws -> NitroModulesPromise<Bool> {
-        return Promise.resolved(withResult: false)
-    }
-    
-    
 }
--- packages/react-native-healthkit/src/specs/Core.nitro.ts
+++ packages/react-native-healthkit/src/specs/Core.nitro.ts
@@ -7,25 +7,41 @@ export interface EmitterSubscription {
 }
 
 export interface OnChangeCallbackArgs {
-    readonly typeIdentifier: SampleTypeIdentifier;
+    readonly typeIdentifier: string;
+    readonly errorMessage?: string;
+}
+
+interface IdentifierWithUnit {
+    typeIdentifier: string;
+    unit: string;
 }
 
 export interface Core extends HybridObject<{ ios: 'swift' }> {
     /**
-       *  @see {@link https://developer.apple.com/documentation/healthkit/hkhealthstore/1614180-ishealthdataavailable Apple Docs }
-       */
+     *  @see {@link https://developer.apple.com/documentation/healthkit/hkhealthstore/1614180-ishealthdataavailable Apple Docs }
+     */
     isHealthDataAvailable(): boolean;
+    isHealthDataAvailableAsync(): Promise<boolean>;
+
     isProtectedDataAvailable(): boolean;
+    isProtectedDataAvailableAsync(): Promise<boolean>;
+
     getPreferredUnits(
-        identifiers: readonly QuantityTypeIdentifier[]
-    ): Promise<Record<string, string>>;
+        identifiers: readonly string[]
+    ): Promise<IdentifierWithUnit[]>;
 
-    addListener(
-        callback: (args: OnChangeCallbackArgs) => void
-    ): EmitterSubscription;
-
+    /** @see {@link https://developer.apple.com/documentation/healthkit/hkhealthstore/1614180-requestauthorizationtoaccess Apple Docs }
+     * @param {SampleTypeIdentifier} typeIdentifier - The type identifier of the sample to request authorization for.
+     * @param {(args: OnChangeCallbackArgs) => void} callback - An array of type identifiers that the app wants to write.
+     * @returns A promise that resolves to a boolean indicating whether the authorization was successful.*/
     subscribeToObserverQuery(
-        identifier: string
-    ): Promise<string>;
-    unsubscribeQuery(queryId: string): Promise<boolean>;
+        /**
+         * @type {SampleTypeIdentifier}
+        */
+        typeIdentifier: string,
+        callback: (args: OnChangeCallbackArgs) => void
+    ): string;
+
+    unsubscribeQuery(queryId: string): boolean;
+    unsubscribeQueryAsync(queryId: string): Promise<boolean>;
 }
