c43816a0660ca795dd9681a18df945112d4fd4ea
Robert Herber <robert@kingstinct.com>
2025-06-16 10:37:49 +0200

    feat: add push-notifications to example app

--- apps/example/app/(tabs)/subscriptions.tsx
+++ apps/example/app/(tabs)/subscriptions.tsx
@@ -3,8 +3,15 @@ import { AllSampleTypesInApp } from "@/constants/AllUsedIdentifiersInApp";
 import { List } from "@expo/ui/swift-ui";
 import { Section, Text } from "@expo/ui/swift-ui-primitives";
 import { useEffect, useState } from "react";
-import { Core } from "@kingstinct/react-native-healthkit";
+import {
+	Core,
+	enableBackgroundDelivery,
+} from "@kingstinct/react-native-healthkit";
 import type { SampleTypeIdentifier } from "@kingstinct/react-native-healthkit/types/Shared";
+import { impactAsync, ImpactFeedbackStyle } from "expo-haptics";
+import { AppState } from "react-native";
+import * as Notifications from "expo-notifications";
+import { UpdateFrequency } from "@kingstinct/react-native-healthkit/types/Background";
 
 const transformIdentifierToName = (identifier: SampleTypeIdentifier) => {
 	return identifier
@@ -18,20 +25,62 @@ const Subscriptions = () => {
 	const [events, setEvents] = useState<
 		{ sampleTypeIdentifier: SampleTypeIdentifier; timestamp: Date }[]
 	>([]);
+
 	useEffect(() => {
-		const subscriptionIds = AllSampleTypesInApp.map((sampleType) => {
-			return Core.subscribeToObserverQuery(sampleType, (args) => {
-				if (args.errorMessage) {
-					alert(
-						`Error in observer query for ${sampleType}: ${args.errorMessage}`,
-					);
-				}
-				setEvents((prevEvents) => [
-					{ sampleTypeIdentifier: args.typeIdentifier, timestamp: new Date() },
-					...prevEvents,
-				]);
+		let subscriptionIds: string[] = [];
+		const init = async () => {
+			const response = await Notifications.requestPermissionsAsync({
+				ios: {
+					provideAppNotificationSettings: true,
+					allowAlert: true,
+					allowSound: true,
+					allowBadge: true,
+				},
 			});
-		});
+
+			subscriptionIds = AllSampleTypesInApp.map((sampleType) => {
+				enableBackgroundDelivery(sampleType, UpdateFrequency.immediate);
+
+				return Core.subscribeToObserverQuery(sampleType, (args) => {
+					if (args.errorMessage) {
+						if (AppState.currentState === "active") {
+							alert(
+								`Error in observer query for ${sampleType}: ${args.errorMessage}`,
+							);
+						} else {
+							Notifications.scheduleNotificationAsync({
+								content: {
+									title: "Error in observer query!",
+									body: sampleType,
+								},
+								trigger: null,
+							});
+						}
+						return;
+					}
+					if (AppState.currentState === "active") {
+						impactAsync(ImpactFeedbackStyle.Light);
+					} else {
+						Notifications.scheduleNotificationAsync({
+							content: {
+								title: "Got a new event!",
+								body: sampleType,
+							},
+							trigger: null,
+						});
+					}
+
+					setEvents((prevEvents) => [
+						{
+							sampleTypeIdentifier: args.typeIdentifier,
+							timestamp: new Date(),
+						},
+						...prevEvents,
+					]);
+				});
+			});
+		};
+		void init();
 
 		return () => {
 			Core.unsubscribeQueries(subscriptionIds);
@@ -44,7 +93,9 @@ const Subscriptions = () => {
 			<Section title="Events">
 				{events.map((event, index) => (
 					<ListItem
-						key={event.sampleTypeIdentifier + event.timestamp.toLocaleTimeString()}
+						key={
+							event.sampleTypeIdentifier + event.timestamp.toLocaleTimeString()
+						}
 						title={transformIdentifierToName(event.sampleTypeIdentifier)}
 						subtitle={`${event.timestamp.toLocaleTimeString()}`}
 					/>
