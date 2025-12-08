c588e049507fffbb6c70e461e4ae287a02d6965d
Robert Herber <robert@kingstinct.com>
2025-12-08 16:27:01 +0800

    feat: lets not use anchored when subscribing to samples

--- packages/react-native-healthkit/src/utils/subscribeToQuantitySamples.ts
+++ packages/react-native-healthkit/src/utils/subscribeToQuantitySamples.ts
@@ -1,4 +1,4 @@
-import { QuantityTypes } from '../modules'
+import { queryQuantitySamples } from '../healthkit'
 import type { QuantityTypeIdentifier } from '../types'
 import type { OnQuantitySamplesCallback } from '../types/Subscriptions'
 import { subscribeToChanges } from './subscribeToChanges'
@@ -6,26 +6,8 @@ import { subscribeToChanges } from './subscribeToChanges'
 export const subscribeToQuantitySamples = (
   identifier: QuantityTypeIdentifier,
   callback: (args: OnQuantitySamplesCallback) => void,
-  after?: Date,
+  after = new Date(),
 ) => {
-  let anchor: string | undefined
-  const afterDate = after ?? new Date()
-
-  const init = async () => {
-    // we need to do an initial query to get a handle for the deletedSamples
-    const { newAnchor } = await QuantityTypes.queryQuantitySamplesWithAnchor(
-      identifier,
-      {
-        filter: {
-          startDate: afterDate,
-        },
-      },
-    )
-    anchor = newAnchor
-  }
-
-  init()
-
   return subscribeToChanges(identifier, async ({ errorMessage }) => {
     if (errorMessage) {
       return callback({
@@ -34,29 +16,17 @@ export const subscribeToQuantitySamples = (
       })
     }
 
-    // seems like all deletedSamples are included when no anchor is provided, so we don't want to return those for the first query
-    const hadAnchorWhenDoingQuery = !!anchor
+    const samplesAfterLast = await queryQuantitySamples(identifier, {
+      limit: 0,
+      filter: {
+        startDate: after,
+      },
+    })
 
-    const { samples, newAnchor, deletedSamples } =
-      await QuantityTypes.queryQuantitySamplesWithAnchor(identifier, {
-        anchor,
-        filter: {
-          startDate: afterDate,
-        },
-      })
-
-    anchor = newAnchor
-
-    const hasNewSamples =
-      samples.length > 0 ||
-      (deletedSamples.length > 0 && hadAnchorWhenDoingQuery)
-
-    if (hasNewSamples) {
+    if (samplesAfterLast.length > 0) {
       callback({
         typeIdentifier: identifier,
-        samples,
-        anchor: newAnchor,
-        deletedSamples: hadAnchorWhenDoingQuery ? deletedSamples : [],
+        samples: samplesAfterLast,
       })
     }
   })
